11ad8afb427ff4b4909e59f35a2088374fdbfb5d
Nikolai Khokhlov <nikolay.khokhlov@simbirsoft.com>
2024-04-07 13:23:18 +0300

    2222

--- src/Components/LeftMenu/LeftMenu.jsx
+++ src/Components/LeftMenu/LeftMenu.jsx
@@ -1,29 +1,35 @@
+import Logo from '../../Constants/icons/logo.svg';
+import styles from './LeftMenu.module.css'
+
 const LeftMenu = () =>{
     return(
-        <div>
-            <img></img>
-            <input></input>
-            <div>
-                <h2>Параметры поиска</h2>
-                <div>
+        <div className={styles.left_menu}>
+            <img className={styles.logo} src={Logo} alt="React Logo" />
+            <input className={styles.left_menu_searcher}></input>
+            <div className={styles.left_menu_filters}>
+                <div className={styles.filter_title}>Параметры поиска</div>
+                <div className={styles.filter_is_exist}>
                     <input type="checkbox"></input>
                     <label>в наличии</label>
                 </div>
-                
-                <h3>Жанр</h3>
-                <div>
-                    <input type="radio"></input>
-                    <label>жанр1</label>
+                <div className={styles.filter_genre}>
+                    <div>Жанр</div>
+                    <div>
+                        <input type="radio"></input>
+                        <label>жанр1</label>
+                    </div>
+                    <div>
+                        <input type="radio"></input>
+                        <label>жанр2</label>
+                    </div>
+                    <div>
+                        <input type="radio"></input>
+                        <label>жанр3</label>
+                    </div>
                 </div>
-                <div>
-                    <input type="radio"></input>
-                    <label>жанр2</label>
+                <div className={styles.filter_button}>
+                    <button>поиск</button>
                 </div>
-                <div>
-                    <input type="radio"></input>
-                    <label>жанр3</label>
-                </div>
-                <button>поиск</button>
             </div>
         </div>
     )
